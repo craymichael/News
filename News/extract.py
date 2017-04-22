56b3234842355c7ce149ccfc5242014bc2c6947d
Zach Carmichael <zjc2920@rit.edu>
2017-04-22 18:37:55 -0400

    Init article retrieval

--- News/extract.py
+++ News/extract.py
@@ -1,4 +1,5 @@
-""" Copyright 2017 Zach Carmichael
+""" ==========================================================================
+    Copyright 2017 Zach Carmichael
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -11,8 +12,10 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+    ==========================================================================
 """
 from boilerpipe.extract import Extractor
+import string
 
 
 def extract_txt_from_url(url):
@@ -33,3 +36,19 @@ def extract_txt_from_html(html):
         Extracted text
     """
     return Extractor('ArticleExtractor', html=html).getText()
+
+
+def normalize_text(text):
+    """ Normalizes text by:
+            - Replace newline with space
+            - Removes punctuation
+            - Makes lowercase
+            - Strip extra whitespace
+    Args:
+        text: String of text to normalize
+    Returns:
+        Normalized text
+    """
+    # Strip newlines, remove punctuation, make lowercase, strip extra whitespace
+    return text.replace('\n', ' ').translate(None, string.punctuation).lower().strip().join(' ')
+
