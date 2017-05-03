c7dcbf3e542f258aee9cdc3367a4f9c62b733368
Zach Carmichael <zjc2920@g.rit.edu>
2017-05-03 18:01:55 -0400

    Script updates, data test

--- News/UCI-News-Classification.py
+++ News/UCI-News-Classification.py
@@ -39,25 +39,22 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
 
 op = OptionParser()
-op.add_option("--report",
-              action="store_true", dest="print_report",
-              help="Print a detailed classification report.")
-op.add_option("--chi2_select",
-              action="store", type="int", dest="select_chi2",
-              help="Select some number of features using a chi-squared test")
-op.add_option("--confusion_matrix",
-              action="store_true", dest="print_cm",
-              help="Print the confusion matrix.")
-op.add_option("--top10",
-              action="store_true", dest="print_top10",
-              help="Print ten most discriminative terms per class"
-                   " for every classifier.")
-op.add_option("--use_hashing",
-              action="store_true",
-              help="Use a hashing vectorizer.")
-op.add_option("--n_features",
-              action="store", type=int, default=2 ** 16,
-              help="Number of features when using the hashing vectorizer.")
+op.add_option('--chi2_select',
+              action='store', type=int, dest='select_chi2',
+              help='Select some number of features using a chi-squared test')
+op.add_option('--full_report',
+              action='store_true', dest='all_details',
+              help='Print a confusion matrix and classification report.')
+op.add_option('--top10',
+              action='store_true', dest='print_top10',
+              help='Print ten most discriminative terms per class '
+                   'for applicable classifiers.')
+op.add_option('--use_hashing',
+              action='store_true',
+              help='Use a hashing vectorizer instead of TF-IDF.')
+op.add_option('--n_features',
+              action='store', type=int, default=2 ** 16,
+              help='Number of features when using the hashing vectorizer.')
 # TODO Options for feature inclusion (i.e. readability score(s), other biases)
 
 
@@ -87,7 +84,8 @@ def enumerate_targets(targets):
 
 def extract_features(X_train, X_test, y_train, Y_test, use_hashing,
                      chi, n_features):
-    """
+    """Extracts features from text.
+
         Args:
             X_train:     Training data features
             X_test:      Testing data features
@@ -140,46 +138,47 @@ def extract_features(X_train, X_test, y_train, Y_test, use_hashing,
     return X_train, X_test, feature_names
 
 
-def benchmark(clf):
+def benchmark(clf, X_train, y_train, X_test, y_test):
     print('_' * 80)
-    print("Training: ")
+    print('Training:')
     print(clf)
     t0 = time()
     clf.fit(X_train, y_train)
     train_time = time() - t0
-    print("train time: %0.3fs" % train_time)
+    print('Train time:    %0.3fs' % train_time)
 
     t0 = time()
     pred = clf.predict(X_test)
     test_time = time() - t0
-    print("test time:  %0.3fs" % test_time)
+    print('Test time:     %0.3fs' % test_time)
 
-    score = metrics.accuracy_score(y_test, pred)
-    print("accuracy:   %0.3f" % score)
+    accuracy = metrics.accuracy_score(y_test, pred)
+    print('Accuracy:      %0.3f' % accuracy)
+    kappa = metrics.cohen_kappa_score(y_test, pred)
+    print('Cohen\'s Kappa: %0.3f' % kappa)
 
     if hasattr(clf, 'coef_'):
-        print("dimensionality: %d" % clf.coef_.shape[1])
-        print("density: %f" % density(clf.coef_))
+        print('Dimensionality: %d' % clf.coef_.shape[1])
+        print('Density:        %f' % density(clf.coef_))
 
         if opts.print_top10 and feature_names is not None:
-            print("top 10 keywords per class:")
+            print('Top 10 keywords per class:')
             for i, label in enumerate(target_names):
                 top10 = np.argsort(clf.coef_[i])[-10:]
-                print(trim("%s: %s" % (label, " ".join(feature_names[top10]))))
+                print('%s: %s' % (label, ' '.join(feature_names[top10])))
         print()
 
-    if opts.print_report:
-        print("classification report:")
+    if opts.all_details:
+        print('Classification Report:')
         print(metrics.classification_report(y_test, pred,
                                             target_names=target_names))
-
-    if opts.print_cm:
-        print("confusion matrix:")
+        print('Confusion Matrix:')
         print(metrics.confusion_matrix(y_test, pred))
 
     print()
     clf_descr = str(clf).split('(')[0]
-    return clf_descr, score, train_time, test_time
+
+    return clf_descr, accuracy, kappa, train_time, test_time
 
 
 opts, args = op.parse_args()
@@ -206,18 +205,18 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 results = []
 for clf, name in (
-        (RidgeClassifier(tol=1e-2, solver="lsqr"), "Ridge Classifier"),
-        (Perceptron(n_iter=50), "Perceptron"),
-        (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive"),
-        (KNeighborsClassifier(n_neighbors=10), "kNN"),
-        (RandomForestClassifier(n_estimators=100), "Random forest")):
-    print('=' * 80)
+        (RidgeClassifier(tol=1e-2, solver='lsqr'), 'Ridge Classifier'),
+        (Perceptron(n_iter=50), 'Perceptron'),
+        (PassiveAggressiveClassifier(n_iter=50), 'Passive-Aggressive'),
+        (KNeighborsClassifier(n_neighbors=10), 'kNN'),
+        (RandomForestClassifier(n_estimators=100), 'Random forest')):
+    print('_' * 80)
     print(name)
     results.append(benchmark(clf))
 
-for penalty in ["l2", "l1"]:
-    print('=' * 80)
-    print("%s penalty" % penalty.upper())
+for penalty in ['l2', 'l1']:
+    print('_' * 80)
+    print('%s penalty' % penalty.upper())
     # Train Liblinear model
     results.append(benchmark(LinearSVC(loss='l2', penalty=penalty,
                                             dual=False, tol=1e-3)))
@@ -227,46 +226,50 @@ for penalty in ["l2", "l1"]:
                                            penalty=penalty)))
 
 # Train SGD with Elastic Net penalty
-print('=' * 80)
-print("Elastic-Net penalty")
+print('_' * 80)
+print('Elastic-Net penalty')
 results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
-                                       penalty="elasticnet")))
+                                       penalty='elasticnet')))
 
 # Train NearestCentroid without threshold
-print('=' * 80)
-print("NearestCentroid (aka Rocchio classifier)")
+print('_' * 80)
+print('NearestCentroid (aka Rocchio classifier)')
 results.append(benchmark(NearestCentroid()))
 
 # Train sparse Naive Bayes classifiers
-print('=' * 80)
-print("Naive Bayes")
+print('_' * 80)
+print('Naive Bayes')
 results.append(benchmark(MultinomialNB(alpha=.01)))
 results.append(benchmark(BernoulliNB(alpha=.01)))
 
-print('=' * 80)
-print("LinearSVC with L1-based feature selection")
+print('_' * 80)
+print('LinearSVC with L1-based feature selection')
 # The smaller C, the stronger the regularization.
 # The more regularization, the more sparsity.
 results.append(benchmark(Pipeline([
-  ('feature_selection', LinearSVC(penalty="l1", dual=False, tol=1e-3)),
+  ('feature_selection', LinearSVC(penalty='l1', dual=False, tol=1e-3)),
   ('classification', LinearSVC())
 ])))
 
 # ********************* PLOTTING **********************
 indices = np.arange(len(results))
 
-results = [[x[i] for x in results] for i in range(4)]
+results = [[x[i] for x in results] for i in range(5)]
 
-clf_names, score, training_time, test_time = results
+clf_names, accuracy, kappa, training_time, test_time = results
 training_time = np.array(training_time) / np.max(training_time)
 test_time = np.array(test_time) / np.max(test_time)
 
-plt.figure(figsize=(12, 8))
-plt.title("Score")
-plt.barh(indices, score, .2, label="score", color='navy')
-plt.barh(indices + .3, training_time, .2, label="training time",
+plt.figure(figsize=(12 * 1.3, 8))
+plt.title('Classifier Scores')
+plt.barh(indices,      accuracy,      .2, label='accuracy',
+         color='navy')
+plt.barh(indices + .3, kappa,         .2, label='kappa',
+         color='g')
+plt.barh(indices + .6, training_time, .2, label='training time',
          color='c')
-plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
+plt.barh(indices + .9, test_time,     .2, label='test time',
+         color='darkorange')
 plt.yticks(())
 plt.legend(loc='best')
 plt.subplots_adjust(left=.25)
